91fbdd8d3e5f9a5339e7c8f2955a341f3a105db7
Thomas Boby <th.boby@gmail.com>
2021-03-18 19:56:48 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+# Cleanup to be "stateless" on startup, otherwise pulseaudio daemon can't start
+rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+
+# Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+
+# Create a virtual audio source; fixed by adding source master and format
+echo "Creating virtual audio source: ";
+pactl load-module module-virtual-source master=auto_null.monitor format=s16le source_name=VirtualMic
+
+# Set VirtualMic as default input source;
+echo "Setting default source: ";
+pactl set-default-source VirtualMic
+
+
 ACCESS_TOKEN=access_token.json
 
 if [ ! -f "/config/$ACCESS_TOKEN" ] && [ -f "/config/$CLIENT_SECRET" ]; then
